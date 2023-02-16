e9c3619a007800a12b9edbb40bdd94c3e2db32b8
Mayank Kumar Patel <72437985+night05fury@users.noreply.github.com>
2023-02-16 22:42:18 +0530

    Day1

---
+++ Problems/Patterns/Pattern3.java
@@ -0,0 +1,34 @@
+package Problems.Patterns;
+/*'
+Geek is very fond of patterns. Once, his teacher gave him a  pattern to solve. He gave Geek an integer n and asked him to build a pattern.
+Help Geek to build a pattern.
+Example 1:
+Input: 5
+Output:
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+Your Task:
+You don't need to input anything. Complete the function printTriangle() which takes  an integer n  as the input parameter and print the pattern.
+Constraints:
+    1<= N <= 20
+ */
+public class Pattern3
+{
+    void printTriangle(int n) {
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=1;j<=i+1;j++)
+            {
+
+                System.out.print(j +" ");
+
+            }
+
+            System.out.println();
+        }
+    }
+}
